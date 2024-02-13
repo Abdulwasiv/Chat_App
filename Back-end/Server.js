f53ac362eb856af368b5bf2c815df8ddfe1e88df
Abdulwasiv <abdwasiv02@gmail.com>
2024-02-14 03:18:35 +0530

    add basic features

--- Back-end/Server.js
+++ Back-end/Server.js
@@ -1,23 +1,37 @@
-import express from 'express'
-import http from 'http'
-import { Socket } from 'socket.io'
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
-const app=express()
-const server=http.createServer(app)
-const io=Socket(server)
+app.use(cors());
 
-io.on('connection' , (socket)=>{
-    console.log("user connected successfully")
+const server = http.createServer(app);
 
-    socket.on('message',(message)=>{
-        io.emit('message' , message)
-    })
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-    socket.on("disconnect" ,()=>{
-        console.log("user disconnect")
-    })
-})
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-app.listen(3000,()=>{
-    console.log("server start")
-})
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Start Server");
+});
